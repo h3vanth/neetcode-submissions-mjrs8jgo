93b656bc8a3e62dfbd28e40e3c393531e3c19d36
Hevanthraj <114860067+h3vanth@users.noreply.github.com>
2026-04-06 14:47:06 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(String.format("%03d", str.length())).append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> strings = new ArrayList<>();
+        while (str.length() != 0) {
+            int length = Integer.parseInt(str.substring(0, 3));
+            strings.add(str.substring(3, length + 3));
+            // 003Hel
+            str = str.substring(length + 3);
+        }
+        return strings;
+    }
+}
